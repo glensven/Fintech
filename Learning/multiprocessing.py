8a70c096d1ff0cd8362dd480c090e7f00dfc051e
Glen Svenningsen <glen.svenningsen@northwestern.edu>
2021-10-01 16:36:57 -0500

    Update

---
+++ Learning/multiprocessing.py
@@ -0,0 +1,21 @@
+import multiprocessing
+from multiprocessing import Process, cpu_count
+
+import time
+
+def counter(num):
+    count = 0
+    while count < num:
+        count += 1
+
+def main():
+    a = Process(target=counter, args=(10000000000, ))
+
+    a.start()
+    a.join()
+
+    print("finished in: ", time.perf_counter(), "seconds")
+
+
+if __name__ == "__main__":
+    main()

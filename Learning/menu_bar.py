3d086c0873b13cd18a5ca194532ad4107a9e2365
Glen Svenningsen <glen.svenningsen@northwestern.edu>
2021-10-12 16:27:39 -0500

    update

--- Learning/menu_bar.py
+++ Learning/menu_bar.py
@@ -6,18 +6,32 @@ def openFile():
 def saveFile():
     print("file has been saved")
 
+def cut():
+    print("You cut some text")
+
+def copy():
+    print("You copied some text")
+
+def paste():
+    print("You pasted some text")
+
 window = Tk()
 
 menuBar = Menu(window)
 
 window.config(menu=menuBar)
 
-fileMenu = Menu(menuBar, tearoff=0)
+fileMenu = Menu(menuBar, tearoff=0,font=("Arial", 15))
 
 menuBar.add_cascade(label="File", menu=fileMenu)
 fileMenu.add_command(label="Open", command=openFile)
 fileMenu.add_command(label="Save", command=saveFile)
 fileMenu.add_separator()
 fileMenu.add_command(label="Exit", command=quit)
+editMenu = Menu(menuBar, tearoff=0,font=("Arial", 15))
+menuBar.add_cascade(label="Edit", menu=editMenu)
+editMenu.add_command(label="Cut", command=cut)
+editMenu.add_command(label="Copy", command=copy)
+editMenu.add_command(label="Paste", command=paste)
 
 window.mainloop()
